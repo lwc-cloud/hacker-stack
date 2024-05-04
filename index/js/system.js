815a8d5b0f4545f40a5b24cd407188dc6e61a2a4
LinWin-Cloud <2382546858@qq.com>
2024-05-04 20:40:56 +0800

    index/

--- index/js/system.js
+++ index/js/system.js
@@ -1,10 +1,7 @@
 
 
 var remote = 'http://127.0.0.1:5555';
-//var remote = 'http://154.9.253.147:5555'
-//var remote = 'http://154.201.83.21:5555'
-//var remote = 'http://154.201.85.154:5555'
-//var remote = 'http://127.0.0.1:5555'
+//var remote = 'http://stack.tiaha.cn:5555'
 
 function to_social() {
     var i = document.getElementById('page');
