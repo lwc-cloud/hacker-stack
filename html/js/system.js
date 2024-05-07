b00d83eff071f6364e04f845f68f379860ceea83
LinWin-Cloud <2382546858@qq.com>
2024-05-07 21:26:35 +0800

    html

--- html/js/system.js
+++ html/js/system.js
@@ -1,7 +1,7 @@
 
 
-var remote = 'http://127.0.0.1:5555';
-//var remote = 'http://stack.tiaha.cn:5555'
+//var remote = 'http://127.0.0.1:5555';
+var remote = 'http://stack.tiaha.cn:5555'
 
 function to_social() {
     var i = document.getElementById('page');
