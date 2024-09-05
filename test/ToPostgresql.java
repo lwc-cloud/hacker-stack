050169c61049b21d72f3e1e183ad35192aa081d8
LinWin-Cloud <2382546858@qq.com>
2024-09-05 14:49:44 +0800

    test

--- test/ToPostgresql.java
+++ test/ToPostgresql.java
@@ -1,2 +1,54 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.TreeMap;
+
 public class ToPostgresql {
+    public static String DBURL = null;
+    public static String DBUserName = null;
+    public static String DBPassword = null;
+
+    public static Object readObjectFromFile(String path) throws Exception{
+        ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(path));
+        Object object = inputStream.readObject();
+        inputStream.close();
+        return object;
+    }
+
+    public static void main(String[] args) throws Exception{
+        Class.forName("org.postgresql.Driver");
+        File file = new File("./db/");
+
+        JsonObject jsonObject = new JsonObject(JsonObject.getFileString("config.json"));
+        DBURL = (String) jsonObject.get("db_url");
+        DBUserName = (String) jsonObject.get("db_user");
+        DBPassword = (String) jsonObject.get("db_pwd");
+
+        String url = DBURL+"/accounts";
+        Connection conn = DriverManager.getConnection(url, DBUserName, DBPassword);
+        Statement stmt = conn.createStatement();
+
+
+        for (File f : file.listFiles()) {
+            if (f.isDirectory()) {
+                continue;
+            }
+            try {
+                TreeMap<String,String> treeMap = (TreeMap<String, String>) readObjectFromFile(f.getAbsolutePath());
+                String username = f.getName().substring(0,f.getName().indexOf("-")).trim();
+                String password = f.getName().substring(f.getName().indexOf("-")+1 , f.getName().lastIndexOf(".")).trim();
+
+                String sqlQuery = "INSERT INTO accounts (username,password,about,apikey) VALUES('"+username+"','"+password+"','"+treeMap.get("about")+"',NULL);";
+                stmt.execute(sqlQuery);
+            }
+            catch (Exception e) {
+                continue;
+            }
+        }
+    }
 }
--- test/test.java
+++ test/test.java
@@ -1,15 +1,15 @@
 import java.io.File;
+import java.sql.*;
 import java.util.TreeMap;
 
 public class test {
-    public static void main(String[] args) throws Exception {
-        //System.out.println(JsonObject.getFileString("1.json"));
-        String json  =JsonObject.getFileString("1.json");
-        long start = System.currentTimeMillis();
-        for (int i = 0 ; i < 1000 ;i++) {
-            JsonObject jsonObject = new JsonObject(json);
-        }
-        long end = System.currentTimeMillis();
-        System.out.println(end-start);
+    public static String DBURL = null;
+    public static String DBUserName = null;
+    public static String DBPassword = null;
+
+    public static void main(String[] args) {
+        // 调用方法并打印结果
+        long nextHourAsInt = ToolKits.getNextHourAsInt() ;
+        System.out.println("下一个小时的时间（以整数形式）: " + nextHourAsInt);
     }
 }
