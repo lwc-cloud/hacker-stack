c61a403e8f793c1bee8e9c78e1b242f2a0ea15ed
LinWin-Cloud <2382546858@qq.com>
2024-09-09 20:33:25 +0800

    mailserver.py

--- mailserver.py
+++ mailserver.py
@@ -2,41 +2,55 @@ from flask import Flask,request
 import json
 import threading
 import time
-import subprocess
+import yagmail
 import flask_cors
+import random
 
 app = Flask(__name__)
 flask_cors.CORS(app)
 
 visit_request = {}
+mail_list = {}
+
+
+
 @app.route('/api/v1/send_mail', methods=['POST','GET'])
 def hello_world():
     # 限制每个IP对制定api的访问.
     client_ip = request.remote_addr
     if client_ip in visit_request:
         visit_request[client_ip] = visit_request[client_ip] + 1
-        if visit_request[client_ip] >= 4:
+        if visit_request[client_ip] > 3:
             return json.dumps({'status': 'error','message': 'Too many requests'}), 429
     else:
         visit_request[client_ip] = 1
 
-    data = request.get_data().decode('utf-8')
-    print(data)
-    json_data = json.loads(data)
-    # 发送邮件
-    send_to = json_data['send_to']
-    subject = json_data['subject']
-    content = json_data['content']
+    try:
+        data = request.get_data().decode('utf-8')
+        print(data)
+        json_data = json.loads(data)
+        # 发送邮件
+        send_to = json_data['send_to']
+        subject = json_data['subject']
+        content = json_data['content']
 
-    from_field = " Linwinsoft账户"
-    command = f"echo '{content}' | mail -s '{subject}' {send_to}"
-    result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
-    if result.returncode == 0:
-        print("邮件发送成功")
-        return json.dumps({'status': 'ok','message': '邮件发送成功'}), 200
-    else:
-        print("邮件发送失败，返回码：", result.returncode)
-        return json.dumps({'status': 'error','message': '邮件发送失败'}), 500
+        # 连接服务器
+        # 用户名、授权码、服务器地址
+        use_mail = random_mail_key()
+        yag_server = yagmail.SMTP(user=use_mail,
+                password=mail_list[use_mail], smtp_ssl=True, host='smtp.163.com')
+
+        # 发送对象列表
+        email_to = [send_to]
+        email_title = subject
+        email_content = content
+        # 发送邮件
+        yag_server.send(email_to, email_title, email_content)
+        # 关闭连接
+        yag_server.close()
+        return json.dumps({'status': 'ok','message': 'Send mail success.'}), 200
+    except Exception as e:
+        return json.dumps({'status': 'error','message': str(e)}), 500
 
 
 def visit_request_threading() -> None:
@@ -44,8 +58,16 @@ def visit_request_threading() -> None:
         time.sleep(60) # 休息 60 秒
         visit_request.clear()
 
+def random_mail_key():
+    mail_list_keys = list(mail_list.keys())
+    random_key = random.choice(mail_list_keys)
+    return random_key
+
 if __name__ == '__main__':
     visit_request_thread = threading.Thread(target=visit_request_threading , name='visit_request')
     visit_request_thread.start()
+
+    mail_list = json.load(open('mail_password.json', 'r'))
+    print(mail_list)
 
     app.run(debug=False, host='0.0.0.0', port=11011)
